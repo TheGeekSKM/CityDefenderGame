35923a82fb7274443024f8c06db1e89ddd8a68ef
Sai <thegeekskm101@gmail.com>
2021-05-26 12:57:25 -0500

    Auto Enemy Spawning
    Enemies Spawned Automatically. There's no bullet collision as of yet, but it will be implemented soon.

---
+++ Assets/Scripts/AlienMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Rigidbody2D rB;
+
+    [Header("Properties")]
+    [SerializeField] public float alienSpeed = 1f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rB.MovePosition(rB.position + new Vector2(0, -1) * alienSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/AlienSpawning.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class AlienSpawning : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform[] spawnPoints;
+    [SerializeField] GameObject alienSpaceShip;
+
+    [Header("Properties")]
+    [SerializeField] public float timeBetweenWaves = 1f;
+
+    //private
+    private float timeToSpawn = 2f;
+
+    
+    void Update()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnShips();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void SpawnShips()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(alienSpaceShip, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
