d766bc9254f638cb717fa790dbeea95482dea167
Sai Mangipudi <thegeekskm101@gmail.com>
2021-05-26 10:45:08 -0500

    Character Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Rigidbody2D rB;
+
+    [Header("Properties")]
+    [SerializeField] public float moveSpeed = 1000f;
+    [SerializeField] public float turnSpeed = 10f;
+
+    //Private Variables
+    Vector2 movement;
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rB.AddForce(movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
