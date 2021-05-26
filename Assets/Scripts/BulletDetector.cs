debf3f14b37162af8313b8b36b8a270160821d38
Sai <thegeekskm101@gmail.com>
2021-05-26 13:13:29 -0500

    Basic Core Loop Setup
    The Aliens spawn, they need to be hit to be killed. They are killed after two hits. Player health and City health have not been established, but they will be next.

---
+++ Assets/Scripts/BulletDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDetector : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] EnemyController eC;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag.Equals("Bullet"))
+    //    {
+    //        eC.BulletDamage(10f);
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Bullet"))
+        {
+            eC.BulletDamage(10f);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] public string shipType = "Crasher";
+    [SerializeField] public float health = 20;
+    void Update()
+    {
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void BulletDamage()
+    {
+        health -= 10f;
+    }
+    public void BulletDamage(float damageNumber)
+    {
+        health -= damageNumber;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
